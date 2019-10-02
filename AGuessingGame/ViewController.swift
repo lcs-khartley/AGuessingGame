6df16e975f649a7b28d447e2eb54fe91c127578e
Keira Hartley <khartley@lcs.on.ca>
2019-10-02 09:57:43 -0400

    Got the computer to speak the opening message.

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    // MARK: Properties (where we store information)
+    
+    // MARK: Initializers
+    
+    // MARK: Methods (functions) -behaviours
+    
+    //Runs as soon as the view becomes visible to the user
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Make an object name 'synthesizer', which is an instance of the class 'AVSpeechSynthesizer'\
+        let synthesizer = AVSpeechSynthesizer()
+        
+        // Make a string that contains what we want the computer to say
+        let message = "I'm thingking of a number between 1 and 100. Guess what it is."
+        
+        // Make an object 'utterance' which is an instance of the class 'AVSpeechUtterance'
+        let utterance = AVSpeechUtterance(string: message)
+        
+        //Speak the message
+        synthesizer.speak(utterance)
+        
     }
 
 
